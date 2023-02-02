a09b1965992fae1b9e3b41d78ad965c50cc2a9df
Jukdang <58977466+jukdang@users.noreply.github.com>
2023-02-02 17:59:07 +0900

    test game

--- kaffle/src/main/frontend/src/board.js
+++ kaffle/src/main/frontend/src/board.js
@@ -1,5 +1,6 @@
 import './board.css'
 import { useState, useEffect, useRef } from 'react';
+import $ from 'jquery';
 
 function Board() {
     
@@ -11,29 +12,106 @@ function Board() {
     // }, []);
 
     const answerList = [
-        ['a', 'b', 'c', 'd', 'e'],
-        ['l', 'm', 'n', 'o', 'j'],
-        ['f', 'g', 'h', 'i', 'k'],
-        ['p', 'q', 'r', 's', 't'],
-        ['y', 'x', 'w', 'v', 'u']
+        ['g', 'a', 'u', 'z', 'e'],
+        ['r', '-', 'p', '-', 'r'],
+        ['a', 'm', 'p', 'l', 'e'],
+        ['v', '-', 'e', '-', 'c'],
+        ['e', 'g', 'r', 'e', 't']
     ]
+    const wordList = ['G', 'P', 'P', 'A', 'E', 'R', 'E', 'Z', 'C', 'U', 'M', 'R', 'R', 'E', 'E', 'A', 'L', 'T', 'G', 'V', 'E'];
+    const rowList = [
+        ['g', 'a', 'u', 'z', 'e'],
+        ['-', '-', '-', '-', '-'],
+        ['a', 'm', 'p', 'l', 'e'],
+        ['-', '-', '-', '-', '-'],
+        ['e', 'g', 'r', 'e', 't']
+    ];
+    const columnList = [
+        ['g', 'r', 'a', 'v', 'e'],
+        ['-', '-', '-', '-', '-'],
+        ['u', 'p', 'p', 'e', 'r'],
+        ['-', '-', '-', '-', '-'],
+        ['e', 'r', 'e', 'c', 't']
+    ];
 
-
+    const [boardSize, setBoardSize] = useState(5); 
     const [isSelected, setIsSelected] = useState(false);
     const [selected, setSelected] = useState(null);
+    const [correctCnt, setCorrectCnt] = useState(0);
+
+    useEffect(() => {
+        setCorrectCnt(0);
+
+        let cnt=1;
+        for(let i=0;i<boardSize;i++){
+            for(let j=0;j<boardSize;j++){
+                if(i*j%2===0){
+                    let area = (i+1)+"/"+(j+1)+"/"+(i+1)+"/"+(j+1);
+                    $('.tile:nth-child('+cnt+')').css({
+                        "grid-area" : area
+                    })
+                    cnt++;
+                }
+                
+            }
+        }
+        console.log("finish to set grid-area");
+            
+        setTimeout(() => {
+            let alertbar = document.getElementsByClassName('alertbar')[0];
+            alertbar.textContent = 'Start!'
+            
+            setTimeout(() => {
+                let elementList = document.getElementsByClassName('tile');
+                for(let element of elementList){
+                    checkTile(element);
+                }
+                console.log('finish to check tile correct');
+
+                alertbar.classList.add('out');
+                let board = document.getElementsByClassName('board')[0];
+                board.style.opacity = '100%';
+            }, 300)
+        }, 500);
+    }, []);
+
+    useEffect(() => {
+        console.log(correctCnt)
+        if(correctCnt == boardSize**2 - parseInt(boardSize/2)**2){
+            gameEnd();
+        }
+    },[correctCnt])
 
     function checkTile(tile){
-        let tile_value = tile.textContent;
+        let tile_value = tile.textContent.toLowerCase();
         let data_pos = JSON.parse(tile.getAttribute("data-pos"));
-        data_pos= Object.values(data_pos);
-        console.log(data_pos[0], data_pos[1])
-        console.log(answerList[data_pos[0]][data_pos[1]])
-        console.log(tile_value)
-        if(tile_value === answerList[data_pos[0]][data_pos[1]].toUpperCase()){
-            tile.style.backgroundColor = 'blue';
-            // tile.disabled = true;
-        }
+        let x = data_pos.x;
+        let y = data_pos.y;
+        // console.log(data_pos[0], data_pos[1])
+        // console.log(answerList[data_pos[0]][data_pos[1]])
+        // console.log(tile_value)
+        // console.log(rowList[x])
+        // console.log(columnList[y])
+        // console.log(rowList[x].includes(tile_value), columnList[y].includes(tile_value))
         
+        tile.classList.remove('in-line');
+        if(tile_value === answerList[x][y]){
+            // console.log('correct');
+            tile.classList.toggle('correct');
+            setCorrectCnt(correctCnt => correctCnt+1);
+            
+        }
+        else if(rowList[x].includes(tile_value) || columnList[y].includes(tile_value)){
+            tile.classList.add('in-line');
+        }
+    }
+    function gameEnd(){
+        let board = document.getElementsByClassName('board')[0];
+        board.style.opacity = '50%';
+        
+        let alertbar = document.getElementsByClassName('alertbar')[0];
+        alertbar.textContent = 'Good!'
+        alertbar.classList.remove('out');
     }
 
     const onClicked = (e) => {
@@ -64,47 +142,25 @@ function Board() {
         }
     }
 
-    useEffect(() => {
-        setTimeout(() => {
-            let alertbar = document.getElementsByClassName('alertbar')[0];
-            alertbar.textContent = 'Start!'
-            
-            setTimeout(() => {
-                let elementList = document.getElementsByClassName('tile');
-                for(let element of elementList){
-                    checkTile(element);
-                }
-
-                alertbar.style.display = 'None';
-                let board = document.getElementsByClassName('board')[0];
-                board.style.opacity = '100%';
-                
-                return () => {
-                    console.log('start game');
-                };
-                }, 300
-            )
-            
-        }, 500);
-      }, []);
-
-    function board_tile(size){
+    function board_tile(){
         let arr = [];
-        let char = 65;
-        for(let i=0;i<size;i++){
-            for(let j=0;j<size;j++){
-                let data_pos = '{"x":'+i+',"y":'+j+'}';
-                arr.push(
-                    <div 
-                        className='tile'
-                        onClick={onClicked}
-                        data-pos={data_pos}
-                        id={char}
-                        key={char}>
-                        {String.fromCharCode(char)}
-                    </div>
-                )
-                char++;
+        let word = 0;
+        for(let i=0;i<boardSize;i++){
+            for(let j=0;j<boardSize;j++){
+                if(i*j%2===0){
+                    let data_pos = '{"x":'+i+',"y":'+j+'}';
+                    arr.push(
+                        <div 
+                            className='tile'
+                            onClick={onClicked}
+                            data-pos={data_pos}
+                            id={word}
+                            key={word}>
+                            {wordList[word]}
+                        </div>
+                    )
+                    word++;
+                }
             }
         }
 
@@ -115,7 +171,7 @@ function Board() {
     return(
         <div className='mainGame'>
             <div className='board'>
-                {board_tile(5)}
+                {board_tile()}
             </div>
             <div className='alertbar'>Ready</div>
         </div>
